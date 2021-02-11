6c606606ece6fdc5cd11e1990a79a622a8a31ad0
Monika <monikadkale98@gmail.com>
2021-02-11 14:47:53 +0530

    added new code

--- Employewagecomputation.java
+++ Employewagecomputation.java
@@ -1,20 +1,32 @@
 public class Employewagecomputation{
 
-public static void main(String[] args)
-{
-        int IS_FULL_TIME =1;
-        int IS_PART_TIME=2;
-        int empwage =20;
-        int emphrs=0;
-        int totalempwage=0;
-        double empcheck=Math.floor(Math.random() * 10 ) % 3 ;
-        if(empcheck == IS_FULL_TIME)
-           emphrs=8;
-        else if(empcheck == IS_PART_TIME)
-          emphrs=4;
-        else
-            emphrs=0;
-        totalempwage=emphrs*empwage;
-        System.out.println("empwage : " +totalempwage );
- }
+   public static void main(String args[])
+    {
+        final int PART_TIME = 1;
+        final int FULL_TIME = 2;
+        final int WAGE_PER_HR = 20;
+        final int WORKING_DAYS = 20;
+
+        int totalWage = 0;
+        for (int day = 1; day <= WORKING_DAYS; day++)
+        {
+            int empType = (int) (Math.random() * 100) % 3;
+            int workingHours =0;
+            switch (empType)
+            {
+                case FULL_TIME:
+                    workingHours = 8;
+                    break;
+                case PART_TIME:
+                    workingHours = 4;
+                    break;
+                default:
+            }
+            int wage = workingHours * WAGE_PER_HR;
+            System.out.println("Day " + day + " wage is:" + wage);
+            totalWage += wage;
+        }
+        System.out.println("Total wage for a month is " + totalWage);        
+    }
+
 }
